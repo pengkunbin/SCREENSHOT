4de46a2701e54fa3680af51a95f3c37aebb2448c
pengkunbin <815371297@qq.com>
2021-01-13 19:25:40 +0800

    feature: add ubuntu puppeteer config

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ if (!url || !imgPath) {
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
   await page.goto(url);
